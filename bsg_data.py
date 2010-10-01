ef111964209f1c8d216bb26a39ff94d4b88dd5d4
Jonathan Claggett <jclaggett@sentryds.com>
2010-09-30 23:49:42 -0400

    added initial bsg_data.py

---
+++ bsg_data.py
@@ -0,0 +1,12 @@
+# This file contains the various data structures for describing the bsg game.
+
+# board
+
+# skill card decks
+
+# president card deck
+
+# event card deck
+
+# loyalty card deck
+
